a428d212f4b2739a7501d7620f77f63ce7059982
Yuliia Mysova <yuliiamysova@gmail.com>
2021-05-16 15:51:13 +0300

    Add project files.

---
+++ HomeTask1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HomeTask1
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 12; i += 2)
+            {
+                for (int j = 0; j < 12 - i - 1; j += 2)
+                {
+                    Console.Write("  ");
+                }
+                for (int k = 12 - i - 1; k < 12; k++)
+                {
+                    Console.Write("*");
+                    Console.Write(" ");
+                }
+                Console.WriteLine(" ");
+            }
+
+        }
+    }
+}
